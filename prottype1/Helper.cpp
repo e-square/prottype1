77bad6cf569763caef64e397632c883638d51091
Kosuke Takeuchi <u651601f@gmail.com>
2013-04-27 05:20:24 +0900

    second commit

---
+++ prottype1/Helper.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+class Helper {
+private:
+    Picture *scanedPicture;
+    cvClass *cvRunner(Picture *scanedPicture, Object obj);
+    Square *squareObject;
+    glClass *glRunner(Object *obj);
+}
+
+public:
+class doScan(Picture *scannedPicture, Object obj)
+{
+    this.cvRunner(Picture *scanedPicture, Object *object);
+    this.glRunner(Object *squareObject);
+}
